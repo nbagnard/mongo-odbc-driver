4c32d44fd84fcf3060b1cb3fba1f02ded0dffa68
Natacha Bagnard <natacha.bagnard@mongodb.com>
2022-11-17 15:23:55 -0800

    Print more

--- odbc/src/api/data.rs
+++ odbc/src/api/data.rs
@@ -997,6 +997,8 @@ pub mod i16_len {
         let message = message.encode_utf16().collect::<Vec<u16>>();
         let (len, ret) = set_output_wstring_helper(&message, output_ptr, buffer_len);
         *text_length_ptr = len as SmallInt;
+        let output_msg = &(String::from_utf16_lossy( & * (output_ptr as * const [u16; 400])))[0..*text_length_ptr as usize];
+        println!("set_output_wstring : {}", output_msg);
         ret
     }
 
