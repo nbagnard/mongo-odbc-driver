ef330e6156e8d54c742c6061ece3b611dae14c8e
Natacha Bagnard <natacha.bagnard@mongodb.com>
2022-11-22 14:18:35 -0800

    Remove outstr and add sqltreturn

--- odbc/src/api/functions.rs
+++ odbc/src/api/functions.rs
@@ -966,7 +966,8 @@ pub unsafe extern "C" fn SQLDriverConnectW(
                 string_length_2,
             );
             file_dbg!(format!("string_length_2 = {}", *string_length_2));
-            file_dbg!(format!("out_connection_string = {}", input_wtext_to_string(out_connection_string, *string_length_2 as usize)));
+            file_dbg!(format!("sql_return = {}", sql_return as usize));
+            //file_dbg!(format!("out_connection_string = {}", input_wtext_to_string(out_connection_string, *string_length_2 as usize)));
             if sql_return == SqlReturn::SUCCESS_WITH_INFO {
                 conn_handle.add_diag_info(ODBCError::OutStringTruncated(buffer_len));
             }
