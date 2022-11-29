ac06ca5e73cfe6c4f261913ef8005576275f9a02
Natacha Bagnard <natacha.bagnard@mongodb.com>
2022-11-28 16:02:52 -0800

    Return success instead of no data

--- odbc/src/api/data.rs
+++ odbc/src/api/data.rs
@@ -1007,7 +1007,7 @@ pub mod i16_len {
         file_dbg!(message);
         let message = message.encode_utf16().collect::<Vec<u16>>();
         let (len, ret) = set_output_wstring_helper(&message, output_ptr, buffer_len);
-        *text_length_ptr = (len * 2) as SmallInt;
+        *text_length_ptr = len as SmallInt;
         let output_msg = &(String::from_utf16_lossy(&*(output_ptr as *const [u16; 400])))
             [0..*text_length_ptr as usize];
         file_dbg!(format!("set_output_wstring : {}", output_msg));
